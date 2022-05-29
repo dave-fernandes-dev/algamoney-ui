218ad2ac5c2f62c3ddec6099f71b6e249bb72702
Dave Fernandes Dev <dave.fernandes.dev@gmail.com>
2022-05-29 17:57:11 -0300

    concluída aula 17.2. Criando o serviço de consulta de lançamentos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -46,6 +47,8 @@ import { CoreModule } from './core/core.module';
     InputMaskModule,
     MessagesModule,
     MessageModule,
+    HttpClientModule,
+
 
     LancamentosModule,
     PessoasModule,
