20c9adddcca980710448c34586b39abce8e7f606
Dave Fernandes Dev <dave.fernandes.dev@gmail.com>
2022-05-29 19:33:58 -0300

    concluída aula 17.3. Adicionando filtro por descrição na pesquisa de lançamentos

--- src/app/lancamentos/lancamento.service.ts
+++ src/app/lancamentos/lancamento.service.ts
@@ -1,20 +1,47 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export interface LancamentoFiltro{
+  descricao: string;
+}
+
 
 @Injectable({
   providedIn: 'root',
 })
 export class LancamentoService {
+
   lancamentosUrl = 'http://localhost:8080/lancamentos';
 
   constructor(private http: HttpClient) {}
 
   pesquisar(): Promise<any> {
+
     const headers = new HttpHeaders().append('Authorization', 'Basic YWRtaW5AYWxnYW1vbmV5LmNvbTphZG1pbg==');
 
     return this.http
-      .get(`${this.lancamentosUrl}?resumo`, { headers })
+      .get(`${this.lancamentosUrl}?resumo`, { headers }) // ou  { headers: headers }
       .toPromise()
       .then((response: any) => response['content']);
   }
+
+
+  pesquisarComFiltro(filtro: LancamentoFiltro): Promise<any> {
+
+    const headers = new HttpHeaders().append('Authorization', 'Basic YWRtaW5AYWxnYW1vbmV5LmNvbTphZG1pbg==');
+
+    let params = new HttpParams();
+
+    if (filtro.descricao) {
+      //Precisamos atribuir o resultado do método "set" novamente à variável "params"
+      params = params.set('descricao', filtro.descricao);
+    }
+
+  return this.http.get(`${this.lancamentosUrl}?resumo`, { headers, params })
+      .toPromise()
+      .then((response: any) => response['content']);
+  }
+
+
 }
--- src/app/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
+++ src/app/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
@@ -1,26 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import { LancamentoService } from '../lancamento.service';
+import { LancamentoService, LancamentoFiltro } from '../lancamento.service';
 
 @Component({
   selector: 'app-lancamentos-pesquisa',
   templateUrl: './lancamentos-pesquisa.component.html',
-  styleUrls: ['./lancamentos-pesquisa.component.scss']
+  styleUrls: ['./lancamentos-pesquisa.component.scss'],
 })
 export class LancamentosPesquisaComponent implements OnInit {
 
-  lancamentos: any[] = [] ;
+  descricao!: string;
+  lancamentos: any[] = [];
   // ou lancamentos = [] ;
-
 
   constructor(private lancamentoService: LancamentoService) {}
 
   ngOnInit(): void {
     this.pesquisar();
-   }
-
-  pesquisar(): void {
-    this.lancamentoService.pesquisar()
-      .then(lancamentos => this.lancamentos = lancamentos);
   }
 
+  pesquisar(): void {
+    this.lancamentoService.pesquisarComFiltro({ descricao: this.descricao })
+    .then(lancamentos => (this.lancamentos = lancamentos));
+  }
 }
