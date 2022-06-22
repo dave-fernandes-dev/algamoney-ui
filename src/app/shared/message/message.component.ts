f1c1b602a7baf1f7902a965febaeb99a3c977a41
Dave Fernandes Dev <dave.fernandes.dev@gmail.com>
2022-06-21 22:27:37 -0300

    concluída aula 21.5. Configurando o HTML do formulário reativo 3.0 + otimizações no messageComponente

--- src/app/shared/message/message.component.ts
+++ src/app/shared/message/message.component.ts
@@ -1,25 +1,30 @@
 import { Component, Input } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { AbstractControl, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-message',
-  template: ` <p-message *ngIf="temErro()" severity="error" text="{{ text }}"></p-message> `,
-  styles: [
-    `
-      .p-message-error {
-        margin: 0;
-        margin-top: 4px;
-        padding: 3px;
-      }
-    `,
-  ],
+  template: `
+    <div *ngIf="temErro()"
+      class="p-message p-message-error">
+      {{ text }}
+    </div>
+  `,
+  styles: [`
+    .p-message-error {
+      margin: 0;
+      margin-top: 4px;
+      padding: 3px;
+    }
+  `]
 })
 export class MessageComponent {
-  @Input() error!: string;
-  @Input() control!: FormControl;
-  @Input() text!: string;
+
+  @Input() error: string = '';
+  @Input() control?: AbstractControl | FormControl | null;
+  @Input() text: string = '';
 
   temErro(): boolean {
-    return this.control.hasError(this.error) && this.control.dirty;
+    return this.control ? this.control.hasError(this.error) && this.control.touched : true;
   }
+
 }
