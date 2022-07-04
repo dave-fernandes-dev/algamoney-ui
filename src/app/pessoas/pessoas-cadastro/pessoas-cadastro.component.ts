4247a6b711d41dad563657dec4657dab788577c0
Dave Fernandes Dev <dave.fernandes.dev@gmail.com>
2022-07-04 18:49:04 -0300

    concluída aula 23.16. Editando contato

--- src/app/pessoas/pessoas-cadastro/pessoas-cadastro.component.ts
+++ src/app/pessoas/pessoas-cadastro/pessoas-cadastro.component.ts
@@ -17,6 +17,7 @@ export class PessoasCadastroComponent implements OnInit {
   pessoa = new Pessoa();
   exibindoFormularioContato!: boolean;
   contato!: Contato;
+  contatoIndex!: number;
 
   constructor(private errorHandler: ErrorHandlerService,
               private pessoaService: PessoaService,
@@ -39,10 +40,17 @@ export class PessoasCadastroComponent implements OnInit {
   prepararNovoContato() {
     this.exibindoFormularioContato = true;
     this.contato = new Contato();
+    this.contatoIndex = this.pessoa.contatos.length;
+  }
+
+  prepararEdicaoContato(contato: Contato, index: number) {
+    this.contato = this.clonarContato(contato);
+    this.exibindoFormularioContato = true;
+    this.contatoIndex = index;
   }
 
   confirmarContato(frm: FormControl) {
-    this.pessoa.contatos.push(this.clonarContato(this.contato));
+    this.pessoa.contatos[this.contatoIndex] = this.clonarContato(this.contato);
 
     this.exibindoFormularioContato = false;
 
