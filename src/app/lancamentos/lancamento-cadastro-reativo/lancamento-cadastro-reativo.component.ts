175b481ee72102b491e20487db9eb76d542ac549
Dave Fernandes Dev <dave.fernandes.dev@gmail.com>
2022-07-15 12:30:38 -0300

    corrigindo o upload

--- src/app/lancamentos/lancamento-cadastro-reativo/lancamento-cadastro-reativo.component.ts
+++ src/app/lancamentos/lancamento-cadastro-reativo/lancamento-cadastro-reativo.component.ts
@@ -1,13 +1,14 @@
+import { Title } from '@angular/platform-browser';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+
 import { ErrorHandlerService } from 'src/app/core/error-handler.service';
 import { CategoriaService } from '../../categorias/categoria.service';
 import { PessoaService } from '../../pessoas/pessoa.service';
 import { Lancamento } from '../../core/model';
 import { LancamentoService } from '../lancamento.service';
 import { MessageService } from 'primeng/api';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-lancamento-cadastro-reativo',
@@ -16,8 +17,8 @@ import { Title } from '@angular/platform-browser';
 })
 export class LancamentoCadastroReativoComponent implements OnInit {
 
-  categorias: any[] = [];
-  pessoas: any[] = [];
+  categorias = [];
+  pessoas = [];
   //lancamento = new Lancamento();
   form!: FormGroup;
   uploadEmAndamento = false;
@@ -38,14 +39,13 @@ export class LancamentoCadastroReativoComponent implements OnInit {
               private formBuilder: FormBuilder,
               ) {}
 
-
   ngOnInit(): void {
 
     this.configForm();
 
-    this.title.setTitle('Novo Lancamento');
-
     const id =this.route.snapshot.params['id'];
+
+    this.title.setTitle('Novo Lancamento');
 
     if (id){
       this.loadLancamento(id);
@@ -56,33 +56,56 @@ export class LancamentoCadastroReativoComponent implements OnInit {
     this.loadPessoas();
   }
 
-  configForm(){
-    this.form = this.formBuilder.group({
-      id: [],
-      tipo: ['RECEITA', Validators.required],
-      dataVencimento: [null, Validators.required],
-      dataPagamento: [],
-      descricao: [null, [Validators.required, Validators.minLength(5)]],
-      valor: [null, Validators.required],
-        pessoa: this.formBuilder.group({
-          id: [null, Validators.required],
-          nome: []
-        }),
-        categoria: this.formBuilder.group({
-          id: [null, Validators.required],
-          nome: []
-        }),
-        observacao: [],
-        anexo: [],
-        urlAnexo: []
+  antesUploadAnexo(_event:any) {
+    this.uploadEmAndamento = true;
+  }
+
+  get uploadHeaders() {
+    return this.lancamentoService.uploadHeaders();
+  }
+
+  get urlUploadAnexo() {
+    return this.lancamentoService.urlUploadAnexo();
+  }
+
+  aoTerminarUploadAnexo(event:any) {
+    const anexo = event.originalEvent.body;
+    //console.log(anexo)
+    this.form.patchValue({ anexo: anexo.nome,  urlAnexo: anexo.url });
+    this.uploadEmAndamento = false;
+  }
+
+  erroUpload(_event: any) {
+    this.uploadEmAndamento = false;
+    this.messageService.add({severity:'error', detail:'Erro ao tentar enviar anexo!'});
+    console.log(_event)
+
+  }
+
+  removerAnexo() {
+    this.form.patchValue({
+      anexo: null,
+      urlAnexo: null
     });
   }
 
+  get nomeAnexo() {
+    //console.log('nomeAnexo')
+    const nome = this.form?.get('anexo')?.value;
+    //console.log(nome)
+    if (nome) {
+      return nome.substring(nome.indexOf('_') + 1, nome.length);
+    }
+
+    return '';
+  }
+
+
   private loadLancamento(id: any) {
     this.lancamentoService.buscarPorCodigo(id)
-      //.then(resultado => this.form.setValue(resultado))
-      .then(resultado => this.form.patchValue(resultado))  //con patchvalue so mapeia o q necessario
-      .catch(erro => this.errorHandler.handle(erro));
+    //.then(resultado => this.form.setValue(resultado))
+    .then(resultado => this.form.patchValue(resultado))  //con patchvalue so mapeia o q necessario
+    .catch(erro => this.errorHandler.handle(erro));
   }
 
   editando(){
@@ -121,18 +144,18 @@ export class LancamentoCadastroReativoComponent implements OnInit {
 
   loadCategorias() {
     return this.categoriaService.listarTodas()
-      .then( itens => {
-         this.categorias = itens.map((c: any) => ({ label: c.nome, value: c.id }));
-      })
-      .catch(erro => this.errorHandler.handle(erro));
+    .then( itens => {
+      this.categorias = itens.map((c: any) => ({ label: c.nome, value: c.id }));
+    })
+    .catch(erro => this.errorHandler.handle(erro));
   }
 
   loadPessoas() {
     return this.pessoaService.listarTodas2()
-      .then( itens => {
-         this.pessoas = itens.map((c: any) => ({ label: c.nome, value: c.id }));
-      })
-      .catch(erro => this.errorHandler.handle(erro));
+    .then( itens => {
+      this.pessoas = itens.map((c: any) => ({ label: c.nome, value: c.id }));
+    })
+    .catch(erro => this.errorHandler.handle(erro));
   }
 
   novo(){
@@ -140,43 +163,36 @@ export class LancamentoCadastroReativoComponent implements OnInit {
     this.router.navigate(['/lancamentos/novo'])
   }
 
-  get uploadHeaders() {
-    return this.lancamentoService.uploadHeaders();
+  mudarUpload(){
+    this.uploadEmAndamento = !this.uploadEmAndamento
   }
 
-  get urlUploadAnexo() {
-    this.uploadEmAndamento = true;
-    return this.lancamentoService.urlUploadAnexo();
-  }
 
-  aoTerminarUploadAnexo(event:any) {
-    const anexo = event.originalEvent.body;
-    console.log(anexo)
-    this.form.patchValue({ anexo: anexo.nome,  urlAnexo: anexo.url });
-    this.uploadEmAndamento = false;
-  }
 
-  erroUpload(_event: any) {
-    this.uploadEmAndamento = false;
-    this.messageService.add({severity:'error', detail:'Erro ao tentar enviar anexo!'});
-  }
 
-  removerAnexo() {
-    this.form.patchValue({
-      anexo: null,
-      urlAnexo: null
+
+  configForm(){
+    this.form = this.formBuilder.group({
+      id: [],
+      tipo: ['RECEITA', Validators.required],
+      dataVencimento: [null, Validators.required],
+      dataPagamento: [],
+      descricao: [null, [Validators.required, Validators.minLength(5)]],
+      valor: [null, Validators.required],
+        pessoa: this.formBuilder.group({
+          id: [null, Validators.required],
+          nome: []
+        }),
+        categoria: this.formBuilder.group({
+          id: [null, Validators.required],
+          nome: []
+        }),
+        observacao: [],
+        anexo: [],
+        urlAnexo: []
     });
   }
 
-  get nomeAnexo() {
-    //console.log('nomeAnexo')
-    const nome = this.form?.get('anexo')?.value;
-    //console.log(nome)
-    if (nome) {
-      return nome.substring(nome.indexOf('_') + 1, nome.length);
-    }
 
-    return '';
-  }
 
 }
